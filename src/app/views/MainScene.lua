eda45c2aee7069df9043af4e980ee984c6ce316b
dkz <drunkz@163.com>
2023-02-17 22:52:34 +0800

    临时保存

--- src/app/views/MainScene.lua
+++ src/app/views/MainScene.lua
@@ -1,16 +1,13 @@
-
 local MainScene = class("MainScene", cc.load("mvc").ViewBase)
 
 function MainScene:onCreate()
     -- add background image
-    display.newSprite("HelloWorld.png")
-        :move(display.center)
-        :addTo(self)
+    display.newSprite("HelloWorld.png"):move(display.center):addTo(self)
 
     -- add HelloWorld label
-    cc.Label:createWithSystemFont("Hello World", "Arial", 40)
-        :move(display.cx, display.cy + 200)
-        :addTo(self)
+    -- cc.Label:createWithSystemFont("Hello World", "Arial", 40):move(display.cx, display.cy + 200):addTo(self)
+    local label = cc.Label:createWithSystemFont("测试看看中文显示效果zenmeyang1231230563", "宋体", 12)
+    label:move(display.cx, display.cy + 200):addTo(self)
 
 end
 
--- src/main.lua
+++ src/main.lua
@@ -1,11 +1,20 @@
-
 cc.FileUtils:getInstance():setPopupNotify(false)
 
 require "config"
 require "cocos.init"
 
 local function main()
+    local director = cc.Director:getInstance()
+    local view = director:getOpenGLView()
+    -- view:setFrameSize(1920, 1080)
+    local framesize = view:getFrameSize()
+    view:setDesignResolutionSize(framesize.width, framesize.height, cc.ResolutionPolicy.FIXED_HEIGHT)
+    print(tostring(framesize.width) .. ":" .. tostring(framesize.height))
     require("app.MyApp"):create():run()
+
+    -- local viewsize = director:getWinSize()
+    -- display.contentScaleFactor = director:getContentScaleFactor()
+    -- display.size               = {width = viewsize.width, height = viewsize.height}
 end
 
 local status, msg = xpcall(main, __G__TRACKBACK__)
